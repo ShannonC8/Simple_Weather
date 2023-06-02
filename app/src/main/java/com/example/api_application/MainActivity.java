a34fb6b3baa65dac0ba268121864aff8b7cff873
shannonc8 <shannoncheng08@gmail.com>
2023-06-02 11:01:37 -0400

    new comments

--- app/src/main/java/com/example/api_application/MainActivity.java
+++ app/src/main/java/com/example/api_application/MainActivity.java
@@ -15,44 +15,53 @@ import java.util.regex.Pattern;
 //https://openweathermap.org/current#name
 public class MainActivity extends AppCompatActivity {
     private static final String url = "https://api.openweathermap.org/data/2.5/weather?q=";
-    private static final String key = "2cbb1a1acfa81cd5920476981398fc5d";
-    private static final String weatherPlace = "Cincinnati";
+    private static final String key = ""; //get your own api key
+    private static final String weatherPlace = "London";
     public String re = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView intro = findViewById(R.id.intro);
+        intro.setText("The weather in " + weatherPlace + " is");
         getWeather(weatherPlace);
     }
 
+    /**
+     * This is the function that will both get the weather and update it to the screen
+     * @param place The city name that we want to choose
+     */
     public void getWeather(String place) {
+        //Creating the URL
         String urlFilled = url + place + "&appid=" + key + "&units=imperial";
+        //We need to create a new thread because android studio has restrictions
+        // on accessing the internet on the main thread
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                String res = "";
                 try {
-                    TextView intro = findViewById(R.id.intro);
-                    intro.setText("The weather in " + weatherPlace + " is");
+                    //setting the textview
                     TextView degrees = findViewById(R.id.degrees);
-                    String result = "";
-                    URL url = new URL(urlFilled);
-                    URLConnection urlConn = url.openConnection();
+                    String result = ""; //the url result
+                    URL url = new URL(urlFilled); //creating new url
+                    URLConnection urlConn = url.openConnection(); //creating url connections
                     BufferedReader rd = new BufferedReader(
-                            new InputStreamReader(urlConn.getInputStream()));
+                            new InputStreamReader(urlConn.getInputStream())); //reading url
                     String line;
-                    while ((line = rd.readLine()) != null) {
+                    while ((line = rd.readLine()) != null) { //putting it into string
                         result += line;
                     }
-                    rd.close();
+                    rd.close(); //close
+                    //regex pattern to search
                     Pattern pattern = Pattern.compile("\"temp\":(\\d+.\\d+)"); //"temp": (\d+.\d+)
                     Matcher matcher = pattern.matcher(result);
-                    if (matcher.find()) {
-                        res = matcher.group(1);
+                    String res = "";
+                    if (matcher.find()) { //if match is found
+                        res = matcher.group(1); //first match is the result
                     }
-                    String finalRes = res;
-                    runOnUiThread(new Runnable() {
+                    String finalRes = res; //much be final in order to use in runOnUIThread
+                    runOnUiThread(new Runnable() { //need to use this because degrees cannot be called in thread
                         @Override
                         public void run() {
                             degrees.setText(finalRes);
